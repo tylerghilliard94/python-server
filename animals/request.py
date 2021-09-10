6c997a7dcdf9394a9270d503d9a6dae2e8c75034
Tyler Hilliard <tylerghilliard@gmail.com>
2021-09-10 15:48:39 -0500

    added post animals

--- animals/request.py
+++ animals/request.py
@@ -23,6 +23,23 @@ ANIMALS = [
 ]
 
 
+def create_animal(animal):
+    # Get the id value of the last animal in the list
+    max_id = ANIMALS[-1]["id"]
+
+    # Add 1 to whatever that number is
+    new_id = max_id + 1
+
+    # Add an `id` property to the animal dictionary
+    animal["id"] = new_id
+
+    # Add the animal dictionary to the list
+    ANIMALS.append(animal)
+
+    # Return the dictionary with `id` property added
+    return animal
+
+
 def get_single_animal(id):
     # Variable to hold the found animal, if it exists
     requested_animal = None
--- request_handler.py
+++ request_handler.py
@@ -1,5 +1,6 @@
+import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from animals import get_all_animals, get_single_animal
+from animals import get_all_animals, get_single_animal, create_animal
 from employees import get_all_employees, get_single_employee
 from locations import get_all_locations, get_single_location
 
@@ -89,13 +90,27 @@ class HandleRequests(BaseHTTPRequestHandler):
     # Here's a method on the class that overrides the parent's method.
     # It handles any POST request.
     def do_POST(self):
-        # Set response code to 'Created'
         self._set_headers(201)
-
         content_len = int(self.headers.get('content-length', 0))
         post_body = self.rfile.read(content_len)
-        response = f"received post request:<br>{post_body}"
-        self.wfile.write(response.encode())
+
+        # Convert JSON string to a Python dictionary
+        post_body = json.loads(post_body)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Initialize new animal
+        new_animal = None
+
+        # Add a new animal to the list. Don't worry about
+        # the orange squiggle, you'll define the create_animal
+        # function next.
+        if resource == "animals":
+            new_animal = create_animal(post_body)
+
+        # Encode the new animal and send in response
+        self.wfile.write(f"{new_animal}".encode())
 
     # Here's a method on the class that overrides the parent's method.
     # It handles any PUT request.
